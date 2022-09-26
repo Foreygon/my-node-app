a819411422a5b2c3923c901f97852be6cd7194b8
Foreygon <111753615+Foreygon@users.noreply.github.com>
2022-09-26 14:30:32 +0200

    nouveau commit

--- app.js
+++ app.js
@@ -3,9 +3,6 @@
 const addition = require('./index');
 
 console.log('hello');
-
-console.log('====================================');
 console.log('second fois');
-console.log('====================================');
 
 console.log(addition (10, 5));
